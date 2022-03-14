9bf3490eab35e2615e9dbee522ab262d6e368cd8
Tristan Stevens <tvut@outlook.com>
2022-03-14 14:17:49 -0400

    added cert

--- WEB_DIR/auth-server/app.js
+++ WEB_DIR/auth-server/app.js
@@ -25,15 +25,13 @@ var fs = require('fs');
 var http = require('http');
 var https = require('https');
 // Certificate
-// const privateKey = fs.readFileSync('/etc/letsencrypt/live/yourdomain.com/privkey.pem', 'utf8');
-// const certificate = fs.readFileSync('/etc/letsencrypt/live/yourdomain.com/cert.pem', 'utf8');
-// const ca = fs.readFileSync('/etc/letsencrypt/live/yourdomain.com/chain.pem', 'utf8');
+const privateKey = fs.readFileSync('/etc/letsencrypt/live/backend.spotifyai.ml/fullchain.pem', 'utf8');
+const certificate = fs.readFileSync('/etc/letsencrypt/live/backend.spotifyai.ml/privkey.pem', 'utf8');
 
-// const credentials = {
-// 	key: privateKey,
-// 	cert: certificate,
-// 	ca: ca
-// };
+const credentials = {
+	key: privateKey,
+	cert: certificate
+};
 
 /**
  * Generates a random string containing numbers and letters
@@ -197,12 +195,12 @@ app.get("/refresh_token", function (req, res) {
 });
 
 const httpServer = http.createServer(app);
-// const httpsServer = https.createServer(credentials, app);
+const httpsServer = https.createServer(credentials, app);
 
 httpServer.listen(80, () => {
 	console.log('HTTP Server running on port 80');
 });
 
-// httpsServer.listen(443, () => {
-// 	console.log('HTTPS Server running on port 443');
-// });
+httpsServer.listen(443, () => {
+	console.log('HTTPS Server running on port 443');
+});
