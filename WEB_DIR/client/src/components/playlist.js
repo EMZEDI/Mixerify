09e78f5c8e21dcd77fef9bc383f3973a13780562
Tristan Stevens <tvut@outlook.com>
2022-03-28 05:31:36 -0400

    updated playlist page for mobile

--- WEB_DIR/client/src/components/playlist.js
+++ WEB_DIR/client/src/components/playlist.js
@@ -33,32 +33,32 @@ export const Playlist = ({ spotifyApi }) => {
   return (
     <div className="min-h-screen flex items-center justify-center">
       <div className="shadow-xl bg-gray-700 text-white w-full min-h-screen">
-        <div className="p-20 bg-gradient-to-r from-emerald-800 via-emerald-900 to-emerald-800 shadow-inner">
-          <div className="space-y-2 mb-4 pl-4 container mx-auto">
-            <Link className="text-emerald-400 mb-4 block text-xl" to="/home">
+        <div className="md:p-20 bg-gradient-to-r from-emerald-800 via-emerald-900 to-emerald-800 shadow-inner">
+          <div className="space-y-2 pb-4 pl-4 container mx-auto">
+            <Link className="text-emerald-400 md:pt-0 pt-2 md:mb-4 block md:text-xl" to="/home">
               &larr; Back Home
             </Link>
-            <img className="w-44 py-4" src={spotify} />
+            <img className="md:w-44 w-28 py-4" src={spotify} />
             {playlistD && (
               <>
                 <div className="flex items-center">
                   {playlistD.images.length > 0 && (
                     <img
-                      className="w-60 h-60 mr-10 shadow-xl"
+                      className="w-28 h-28 md:w-60 md:h-60 mr-10 shadow-xl"
                       src={playlistD.images[0].url}
                     />
                   )}
                   <div className="space-y-2">
-                    <h1 className="text-3xl">
+                    <h1 className="md:text-3xl">
                       Your playlist has been generated!
                     </h1>
-                    <h2 className="text-5xl font-semibold">{playlistD.name}</h2>
-                    <h4 className="text-2xl">{playlistD.description}</h4>
-                    <h4 className="text-2xl pb-6">
+                    <h2 className="md:text-5xl font-semibold">{playlistD.name}</h2>
+                    <h4 className="md:text-2xl">{playlistD.description}</h4>
+                    <h4 className="md:text-2xl pb-6">
                       {playlistD.tracks.total} songs
                     </h4>
                     <a
-                      className="transition-all px-6 py-4 text-white bg-[#1DB954] rounded-full hover:bg-emerald-500"
+                      className="text-sm transition-all px-3 md:px-6 py-2 md:py-4 text-white bg-[#1DB954] rounded-full hover:bg-emerald-500"
                       href={playlistD.external_urls.spotify}
                       rel="noopener noreferrer"
                     >
@@ -73,13 +73,13 @@ export const Playlist = ({ spotifyApi }) => {
         <div className="bg-black shadow-xl">
           <div className="container mx-auto">
             <div>
-              <div className="grid grid-cols-11 p-4 border-b-4 border-gray-600">
+              <div className="grid grid-cols-11 p-2 md:p-4 border-b-4 border-gray-600">
                 <div className="col-span-2"></div>
                 <div className="ml-8 flex flex-col justify-center col-span-5">
-                  <h3 className="text-2xl">Song</h3>
+                  <h3 className="md:text-2xl">Song</h3>
                 </div>
                 <div className=" flex flex-col justify-center col-span-4">
-                  <h4 className="text-2xl">Artist</h4>
+                  <h4 className="md:text-2xl">Artist</h4>
                 </div>
               </div>
             </div>
@@ -90,23 +90,23 @@ export const Playlist = ({ spotifyApi }) => {
                     key={s.track.uri}
                     className="grid grid-cols-11 p-3 border-b-2 border-gray-600"
                   >
-                    <div className="h-16 flex justify-center items-center">
+                    <div className="invisible md:visible md:h-16 flex justify-center items-center">
                       <AudioButton
                         key={s.track.uri}
                         audio={s.track.preview_url}
                       />
                     </div>
-                    <div className="h-16">
+                    <div className="-ml-6 md:ml-0 h-16 w-16">
                       <img
                         className="w-16 h-16"
                         src={s.track.album.images[0].url}
                       />
                     </div>
                     <div className="ml-8 h-16 flex flex-col justify-center col-span-5">
-                      <h3 className="text-1xl">{s.track.name}</h3>
+                      <h3 className="md:text-1xl">{s.track.name}</h3>
                     </div>
                     <div className="h-16 flex flex-col justify-center col-span-4">
-                      <h4 className="text-1xl">{s.track.artists[0].name}</h4>
+                      <h4 className="md:text-1xl">{s.track.artists[0].name}</h4>
                     </div>
                   </div>
                 ))}
