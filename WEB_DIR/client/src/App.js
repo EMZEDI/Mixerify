d130bb5f3ebded3e3af2298f76051184e9ce9e5c
Tristan Stevens <tvut@outlook.com>
2022-03-28 04:26:19 -0400

    added 404

--- WEB_DIR/client/src/App.js
+++ WEB_DIR/client/src/App.js
@@ -1,12 +1,29 @@
 import Login from "./components/login";
 import Home from "./components/home";
-import { BrowserRouter, Route, Routes} from 'react-router-dom'
-import './App-compiled.css';
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  Link,
+  Navigate,
+  useLocation,
+} from "react-router-dom";
+import "./App-compiled.css";
 import { useState, useEffect } from "react";
 import Auth from "./components/auth";
 import Playlist from "./components/playlist";
 import SpotifyWebApi from "spotify-web-api-js";
 import Generate from "./components/generate";
+import PageNotFound from "./components/404";
+
+function RequireAuth({ token, children }) {
+  let location = useLocation();
+  if (!token) {
+    // Redirect to the /login page
+    return <Navigate to="/" state={{ from: location }} replace />;
+  }
+  return children
+}
 
 function App() {
   const [token, setToken] = useState("");
@@ -14,15 +31,57 @@ function App() {
   const [playlists, setPlaylists] = useState(null);
   var spotifyApi = new SpotifyWebApi();
   return (
-        <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Login setUser={setUser} setToken={setToken} setPlaylists={setPlaylists} spotifyApi={spotifyApi}/>}/>
-          <Route path="/auth" element={<Auth setToken={setToken} spotifyApi={spotifyApi}/>}/>
-          <Route path="/home" element={<Home user={user} setUser={setUser} playlists={playlists} setPlaylists={setPlaylists} spotifyApi={spotifyApi} token={token}/>} />
-          <Route path="/home/playlist/:id" element={<Playlist spotifyApi={spotifyApi}/>} />
-          <Route path="/home/generate/:id" element={<Generate spotifyApi={spotifyApi} token={token}/>} />
-        </Routes>
-      </BrowserRouter>
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <Login
+              setUser={setUser}
+              setToken={setToken}
+              setPlaylists={setPlaylists}
+              spotifyApi={spotifyApi}
+            />
+          }
+        />
+        <Route
+          path="/auth"
+          element={<Auth setToken={setToken} spotifyApi={spotifyApi} />}
+        />
+        <Route
+          path="/home"
+          element={
+            <RequireAuth token={token}>
+              <Home
+                user={user}
+                setUser={setUser}
+                playlists={playlists}
+                setPlaylists={setPlaylists}
+                spotifyApi={spotifyApi}
+                token={token}
+              />
+            </RequireAuth>
+          }
+        />
+        <Route
+          path="/home/playlist/:id"
+          element={
+            <RequireAuth token={token}>
+              <Playlist spotifyApi={spotifyApi} />
+            </RequireAuth>
+          }
+        />
+        <Route
+          path="/home/generate/:id"
+          element={
+            <RequireAuth token={token}>
+              <Generate spotifyApi={spotifyApi} token={token} />
+            </RequireAuth>
+          }
+        />
+        <Route path="*" element={<PageNotFound/>} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
